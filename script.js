a2c2b3c94b12f1307d687b2c2a38a265b0829916
katstews <112781868+katstews@users.noreply.github.com>
2023-11-27 21:37:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,244 @@
+var human = -1;
+var computer = 1;
+
+// =========== heuristic eval of state =================================================
+function eval(state) {
+	var score = 0;
+
+	if (checkState(state, computer)) {
+		score = 1;
+	}
+	else if (checkState(state, human)) {
+		score = -1;
+	} else {
+		score = 0;
+	}
+
+	return score;
+}
+// ======================================================================================
+
+// ==== return if player won =============================================================
+function checkState(state, player) {
+	var winning_state = [
+		[state[0][0], state[0][1], state[0][2]],
+		[state[1][0], state[1][1], state[1][2]],
+		[state[2][0], state[2][1], state[2][2]],
+		[state[0][0], state[1][0], state[2][0]],
+		[state[0][1], state[1][1], state[2][1]],
+		[state[0][2], state[1][2], state[2][2]],
+		[state[0][0], state[1][1], state[2][2]],
+		[state[2][0], state[1][1], state[0][2]],
+	];
+
+	for (var i = 0; i < 8; i++) {
+		var line = winning_state[i];
+		var filled = 0;
+		for (var j = 0; j < 3; j++) {
+			if (line[j] == player)
+				filled++;
+		}
+		if (filled == 3)
+			return true;
+	}
+	return false;
+}
+// =======================================================================================
+
+// return if human or AI won =============================================================
+function getWinner(state) {
+	return checkState(state, human) || checkState(state, computer);
+}
+// =======================================================================================
+
+// ==== return the coordinates of empty cells in a 3x3 grid =========================
+function clearCells(state) {
+	var cells = [];
+	for (var x = 0; x < 3; x++) {
+		for (var y = 0; y < 3; y++) {
+			if (state[x][y] == 0)
+				cells.push([x, y]);
+		}
+	}
+
+	return cells;
+}
+// =====================================================================================
+
+var board = [
+	[0, 0, 0],
+	[0, 0, 0],
+	[0, 0, 0],
+];
+
+// == is cell empty, make a move (check if valid move or not) ==========================
+function validMove(x, y) {
+	var empties = clearCells(board);
+	try {
+		if (board[x][y] == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+// ====================================================================================
+
+// ============= move on the board if the coordinates are valid =====================
+function setMove(x, y, player) {
+	if (validMove(x, y)) {
+		board[x][y] = player;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+// ===================================================================================
+
+// ======================  official MINIMAX function ==================================
+function minimax(state, depth, player) {
+	var best;
+
+	if (player == computer) {
+		best = [-1, -1, -1000];
+	}
+	else {
+		best = [-1, -1, +1000];
+	}
+
+	if (depth == 0 || getWinner(state)) {
+		var score = eval(state);
+		return [-1, -1, score];
+	}
+
+	clearCells(state).forEach(function (cell) {
+		var x = cell[0];
+		var y = cell[1];
+		state[x][y] = player;
+		var score = minimax(state, depth - 1, -player);
+		state[x][y] = 0;
+		score[0] = x;
+		score[1] = y;
+
+		if (player == computer) {
+			if (score[2] > best[2])
+				best = score;
+		}
+		else {
+			if (score[2] < best[2])
+				best = score;
+		}
+	});
+
+	return best;
+}
+// =======================================================================================
+
+// ================== ai calls minimax function ==========================================
+function aiTurn() {
+	var x, y;
+	var move;
+	var cell;
+
+	if (clearCells(board).length == 9) {
+		x = parseInt(Math.random() * 3);
+		y = parseInt(Math.random() * 3);
+	}
+	else {
+		move = minimax(board, clearCells(board).length, computer);
+		x = move[0];
+		y = move[1];
+	}
+
+	if (setMove(x, y, computer)) {
+		cell = document.getElementById(String(x) + String(y));
+		cell.innerHTML = "O";
+	}
+}
+// ======================================================================================
+
+// ===============  Restart the game (html calls) =======================================
+function restartBnt(button) {
+	if (button.value == "AI Player") {
+		aiTurn();
+		button.disabled = true;
+	}
+	else if (button.value == "Restart") {
+		var htmlBoard;
+		var msg;
+
+		for (var x = 0; x < 3; x++) {
+			for (var y = 0; y < 3; y++) {
+				board[x][y] = 0;
+				htmlBoard = document.getElementById(String(x) + String(y));
+				htmlBoard.style.color = "grey";
+				htmlBoard.innerHTML = "";
+			}
+		}
+		button.value = "AI Player";
+		msg = document.getElementById("message");
+		msg.innerHTML = "";
+	}
+}
+// =======================================================================================
+
+// ================== MAIN ===============================================================
+function clickedCell(cell) { // main calls this
+	var button = document.getElementById("bnt-restart");
+	button.disabled = true;
+	var conditionToContinue = getWinner(board) == false && clearCells(board).length > 0;
+
+	if (conditionToContinue == true) {
+		var x = cell.id.split("")[0];
+		var y = cell.id.split("")[1];
+		var move = setMove(x, y, human);
+		if (move == true) {
+			cell.innerHTML = "X";
+			if (conditionToContinue)
+				aiTurn();
+		}
+	}
+	if (checkState(board, computer)) {
+		var lines;
+		var cell;
+		var msg;
+
+		if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
+			lines = [[0, 0], [0, 1], [0, 2]];
+		else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1)
+			lines = [[1, 0], [1, 1], [1, 2]];
+		else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1)
+			lines = [[2, 0], [2, 1], [2, 2]];
+		else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1)
+			lines = [[0, 0], [1, 0], [2, 0]];
+		else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1)
+			lines = [[0, 1], [1, 1], [2, 1]];
+		else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1)
+			lines = [[0, 2], [1, 2], [2, 2]];
+		else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1)
+			lines = [[0, 0], [1, 1], [2, 2]];
+		else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1)
+			lines = [[2, 0], [1, 1], [0, 2]];
+
+		for (var i = 0; i < lines.length; i++) {
+			cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]));
+			cell.style.color = "red";
+		}
+
+		msg = document.getElementById("message");
+		msg.innerHTML = "You lose!";
+	}
+	if (clearCells(board).length == 0 && !getWinner(board)) {
+		var msg = document.getElementById("message");
+		msg.innerHTML = "Draw!";
+	}
+	if (getWinner(board) == true || clearCells(board).length == 0) {
+		button.value = "Restart";
+		button.disabled = false;
+	}
+}
+// =========================================================================================
